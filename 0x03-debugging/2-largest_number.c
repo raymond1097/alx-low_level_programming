8ebd17febf3d8c22addc9b63a1765b773da86459
raymond1097 <kajogbolaabdulrahmonbolaji@gmail.com>
2022-12-15 02:44:00 -0800

    Three integers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,20 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a >= b && a >= c)
 	{
-		if (b > c)
-			largest = a;
-		else if (a > c)
-			largest = a;
-		else
-			largest = c;
+		largest = a;
 	}
-	else
+	else if (b >= a && b >= c)
 	{
-		if (b < c)
-			largest = c;
-		else
-			largest =  b;
+		largest = b;
 	}
+	else if (c >= b && c >= a)
+	{
+		largest = c;
+	}
+	return (largest);
 }
