6b855ecf07a4b6753bdda60a1ca3a212abb9ad5e
raymond1097 <kajogbolaabdulrahmonbolaji@gmail.com>
2023-01-16 05:49:13 -0800

    pi

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,11 +1,3 @@
-#include <stdio.h>
-/**
- * main - Program compilation starts from the main
- * Description: this program prints the name of the file it was comipiled from
- * Return: 0 (success)
- */
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
+#ifndef PI
+#define PI 3.14159265359
+#endif
